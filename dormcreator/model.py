11d9438c88765960e19262988ece893e0e4bd941
CSSI <css@CSSs-MacBook-Air.local>
2017-08-02 16:23:53 -0500

    fun

--- dormcreator/model.py
+++ dormcreator/model.py
@@ -2,7 +2,6 @@ from google.appengine.ext import ndb
 
 class User(ndb.Model):
     user_id = ndb.StringProperty()
-    password = ndb.StringProperty()
     color = ndb.StringProperty()
     style = ndb.StringProperty()
     gender = ndb.StringProperty()
